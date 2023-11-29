07a8d34db2748c98cdbd7c77d4a0e0e1ea1abbb7
Jeremias Villane <vil.jeremias@gmail.com>
2023-11-28 22:04:57 -0300

    changed filename remove-note-recursively -> remove-file-recursively

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,4 @@
 export * from "./add-file-recursively";
 export * from "./get-focus";
 export * from "./handle-save";
-export * from "./remove-note-recursively";
+export * from "./remove-file-recursively";
