b4326f6c3b114201e71c3435c28c4e33a8300900
Jeremias Villane <vil.jeremias@gmail.com>
2023-12-07 18:20:40 -0300

    refactor: getUserAppFiles

--- src/utils/get-user-app-files.ts
+++ src/utils/get-user-app-files.ts
@@ -7,39 +7,37 @@ export async function getUserAppFiles(path: string) {
   const scanDir = async (path: string): Promise<FileObj[]> => {
     const folderContent: FileEntry[] = await readDir(path);
 
-    let folders: FileObj[] = [];
+    const folders: FileObj[] = [];
     const files: FileObj[] = [];
 
-    await Promise.all(
-      folderContent.map(async (file) => {
-        const id = nanoid();
-        const isFolder: boolean = "children" in file;
-        let subFolder: string | null = null;
+    for (const file of folderContent) {
+      const id = nanoid();
+      const isFolder: boolean = "children" in file;
+      let subFolder: string | null = null;
 
-        if (isFolder) {
-          subFolder = await join(path, file.name!);
-          folders.push({
-            id,
-            name: file.name ?? "",
-            path: file.path,
-            isFolder,
-            children: isFolder ? await scanDir(subFolder!) : undefined,
-          });
-        } else {
-          files.push({
-            id,
-            name: file.name?.split(".")[0] ?? "",
-            path: file.path,
-            isFolder,
-          });
-        }
-      })
-    );
+      if (isFolder) {
+        subFolder = await join(path, file.name!);
+        folders.push({
+          id,
+          name: file.name ?? "",
+          path: file.path,
+          isFolder,
+          children: isFolder ? await scanDir(subFolder!) : undefined,
+        });
+      } else {
+        files.push({
+          id,
+          name: file.name?.split(".")[0] ?? "",
+          path: file.path,
+          isFolder,
+        });
+      }
+    }
 
-    return [
-      ...folders.sort((a, b) => a.name.localeCompare(b.name)),
-      ...files.sort((a, b) => a.name.localeCompare(b.name)),
-    ];
+    folders.sort((a, b) => a.name.localeCompare(b.name));
+    files.sort((a, b) => a.name.localeCompare(b.name));
+
+    return [...folders, ...files];
   };
 
   return await scanDir(path);
