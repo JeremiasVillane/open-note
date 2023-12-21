b6fbdc6c419ab6224148e550228895a6d988be48
Jeremias Villane <vil.jeremias@gmail.com>
2023-12-21 19:19:03 -0300

    chore(render-modal): added JSDoc comment

--- src/helpers/render-modal.ts
+++ src/helpers/render-modal.ts
@@ -1,5 +1,15 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
+/**
+ * Renders a modal with the specified label, width, and height.
+ *
+ * @param {object} props - The props object.
+ * @param {string} props.label - The label to display in the modal.
+ * @param {number} [props.width=400] - The width of the modal in pixels.
+ * @param {number} [props.height=175] - The height of the modal in pixels.
+ * @param {function} props.setActiveModal - A function to set the active state of the modal.
+ * @return {Promise<void>} - A promise that resolves when the modal is rendered.
+ */
 export const renderModal = async ({
   label,
   width = 400,
@@ -10,7 +20,7 @@ export const renderModal = async ({
   width?: number;
   height?: number;
   setActiveModal: (value: boolean) => void;
-}) => {
+}): Promise<void> => {
   await invoke("open_window", { label, width, height });
 
   setActiveModal(true);
